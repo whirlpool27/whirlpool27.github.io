3edb053c8cf9c58cdd48faca8f6096134983d91b
Gilang Anggara <whirlpool27@gmail.com>
2018-09-04 14:39:55 +0700

    Change menu to side navigation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 // Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const sideNav = document.querySelector('.side-nav');
-const main = document.getElementById('home');
+const navItem = document.querySelector('.nav-item');
+const home = document.querySelector('.home');
+const work = document.querySelector('.work');
+const about = document.querySelector('.about');
+const contact = document.querySelector('.contact');
 
 // Set Initial State Of Menu
 var menuVisibility = false;
+
+// store current content id
+// home = 0
+// work = 1
+// about = 2
+// contact = 3
+var currentContent = 0;
 
 menuBtn.addEventListener('click', toggleMenu);
 
@@ -17,24 +28,51 @@ function toggleMenu() {
   }
 }
 
-function openHome() {
-  main.style.display = 'block';
-  hideMenu();
+function openNavLink(destinationID) {
+  hideContent();
+  displayContent(destinationID);
+  currentContent = destinationID;
+  toggleMenu();
 }
 
-function openAbout() {
-  hideContent(main);
-  hideMenu();
+function hideContent() {
+  if (currentContent == 0) {
+    home.style.height = '0';
+  } else if (currentContent == 1) {
+    about.style.height = '0';
+  } else if (currentContent == 2) {
+    work.style.height = '0';
+  } else {
+    contact.style.height = '0';
+  }
 }
 
-function hideContent(id) {
-  id.style.display = 'none';
+function displayContent(id) {
+  if (id == 0) {
+    // home.style.display = 'block';
+    home.style.height = 'auto';
+  } else if (id == 1) {
+    about.style.height = 'auto';
+  } else if (id == 2) {
+    work.style.height = 'auto';
+  } else {
+    contact.style.height = 'auto';
+  }
 }
 
 function showMenu() {
   menuBtn.classList.add('close');
   sideNav.style.width = '20%';
-  main.style.marginLeft = '20%';
+  switch (currentContent) {
+    case 0:
+      home.style.marginLeft = '20%';
+    case 1:
+      about.style.marginLeft = '20%';
+    case 2:
+      work.style.marginLeft = '20%';
+    case 3:
+      contact.style.marginLeft = '20%';
+  }
 
   // Set Menu State
   menuVisibility = true;
@@ -43,7 +81,16 @@ function showMenu() {
 function hideMenu() {
   menuBtn.classList.remove('close');
   sideNav.style.width = '0';
-  main.style.marginLeft = '0';
+  switch (currentContent) {
+    case 0:
+      home.style.marginLeft = '0';
+    case 1:
+      about.style.marginLeft = '0';
+    case 2:
+      work.style.marginLeft = '0';
+    case 3:
+      contact.style.marginLeft = '0';
+  }
 
   // Set Menu State
   menuVisibility = false;
